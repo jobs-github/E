f489afdaf5c542e8c920f9a617e55d50dcf63bec
jobs <yao050421103@163.com>
2022-07-29 22:59:41 +0800

    remove Env.Assign

--- object/env.go
+++ object/env.go
@@ -1,15 +1,8 @@
 package object
 
-import (
-	"fmt"
-
-	"github.com/jobs-github/escript/function"
-)
-
 type Env interface {
 	Get(name string) (Object, bool)
 	Set(name string, val Object) Object
-	Assign(name string, val Object) error
 	NewEnclosedEnv() Env
 }
 
@@ -38,20 +31,6 @@ func (this *environment) Get(name string) (Object, bool) {
 func (this *environment) Set(name string, val Object) Object {
 	this.e[name] = val
 	return val
-}
-
-func (this *environment) Assign(name string, val Object) error {
-	if _, ok := this.e[name]; ok {
-		this.e[name] = val
-	} else {
-		if nil == this.parent {
-			err := fmt.Errorf("`%v` undefined", name)
-			return function.NewError(err)
-		} else {
-			return this.parent.Assign(name, val)
-		}
-	}
-	return nil
 }
 
 func (this *environment) NewEnclosedEnv() Env {
