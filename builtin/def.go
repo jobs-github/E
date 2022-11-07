a1e9d649ebcbde600e26e72a7b3cc08c40627bc2
jobs <yao050421103@163.com>
2022-11-07 22:09:01 +0800

    remove unused

--- builtin/def.go
+++ builtin/def.go
@@ -1,42 +1,10 @@
 package builtin
 
 import (
-	"errors"
 	"fmt"
 	"strconv"
 
 	"github.com/jobs-github/escript/object"
-)
-
-var (
-	errMode = errors.New(`mode string must begin with one of "r", "w", "a", "r+", "w+", "a+"`)
-)
-
-const (
-	// read: Open file for input operations. The file must exist.
-	modeRead = "r"
-	// write: Create an empty file for output operations.
-	// If a file with the same name already exists, its contents are discarded and
-	// the file is treated as a new empty file.
-	modeWrite = "w"
-	// append: Open file for output at the end of a file. Output operations always
-	// write data at the end of the file, expanding it.
-	// Repositioning operations (fseek, fsetpos, rewind) are ignored.
-	// The file is created if it does not exist.
-	modeAppend = "a"
-	// read/update: Open a file for update (both for input and output).
-	// The file must exist.
-	modeReadUpdate = "r+"
-	// write/update: Create an empty file and open it for update (both for input and output).
-	// If a file with the same name already exists its contents are discarded and
-	// the file is treated as a new empty file.
-	modeWriteUpdate = "w+"
-	// append/update: Open a file for update (both for input and output) with
-	// all output operations writing data at the end of the file.
-	// Repositioning operations (fseek, fsetpos, rewind) affects the next
-	// input operations, but output operations move the position back to the end
-	// of file. The file is created if it does not exist.
-	modeAppendUpdate = "a+"
 )
 
 type formatArgs struct {
