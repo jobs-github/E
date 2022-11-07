92f0bd7760b1d676af0fef1ac4659fcceb5262dd
jobs <yao050421103@163.com>
2022-11-07 22:28:49 +0800

    update builtin

--- builtin/builtin.go
+++ builtin/builtin.go
@@ -4,20 +4,45 @@ import (
 	"github.com/jobs-github/escript/object"
 )
 
-var builtins = map[string]object.Object{
-	object.FnLen: object.NewBuiltin(builtinLen, object.FnLen),
-	"type":       object.NewBuiltin(builtinType, "type"),
-	"str":        object.NewBuiltin(builtinStr, "str"),
-	"int":        object.NewBuiltin(builtinInt, "int"),
-	"print":      object.NewBuiltin(builtinPrint, "print"),
-	"println":    object.NewBuiltin(builtinPrintln, "println"),
-	"printf":     object.NewBuiltin(builtinPrintf, "printf"),
-	"sprintf":    object.NewBuiltin(builtinSprintf, "sprintf"),
-	"loads":      object.NewBuiltin(builtinLoads, "loads"),
-	"dumps":      object.NewBuiltin(builtinDumps, "dumps"),
-	"for":        object.NewBuiltin(builtinFor, "for"),
-	"state":      object.NewBuiltin(builtinState, "state"),
+type Builtin struct {
+	name string
+	fn   object.Object
 }
+
+func newBuiltin(name string, fn object.BuiltinFunction) *Builtin {
+	return &Builtin{
+		name: name,
+		fn:   object.NewBuiltin(fn, name),
+	}
+}
+
+type BuiltinSlice []*Builtin
+
+func (this *BuiltinSlice) newMap() map[string]object.Object {
+	m := map[string]object.Object{}
+	for _, v := range *this {
+		m[v.name] = v.fn
+	}
+	return m
+}
+
+var (
+	builtinslice = BuiltinSlice{
+		newBuiltin(object.FnLen, builtinLen),
+		newBuiltin("type", builtinType),
+		newBuiltin("str", builtinStr),
+		newBuiltin("int", builtinInt),
+		newBuiltin("print", builtinPrint),
+		newBuiltin("println", builtinPrintln),
+		newBuiltin("printf", builtinPrintf),
+		newBuiltin("sprintf", builtinSprintf),
+		newBuiltin("loads", builtinLoads),
+		newBuiltin("dumps", builtinDumps),
+		newBuiltin("for", builtinFor),
+		newBuiltin("state", builtinState),
+	}
+	builtins = builtinslice.newMap()
+)
 
 func IsBuiltin(key string) bool {
 	_, ok := builtins[key]
