79cc30e6e948cf71913d4cf823b9dd37b14a7709
jobs <yao050421103@163.com>
2023-03-23 20:25:05 +0800

    merge stmt

--- stmt/stmt.go
+++ stmt/stmt.go
@@ -1,7 +1,11 @@
 package stmt
 
 import (
+	"fmt"
+
 	"github.com/jobs-github/escript/ast"
+	"github.com/jobs-github/escript/builtin"
+	"github.com/jobs-github/escript/function"
 	"github.com/jobs-github/escript/interfaces"
 	"github.com/jobs-github/escript/scanner"
 	"github.com/jobs-github/escript/token"
@@ -57,4 +61,83 @@ func (this *stmtParser) Decode(t token.TokenType, endTok token.TokenType) (ast.S
 
 func (this *stmtParser) isFunctionStmt() bool {
 	return this.scanner.ExpectCur2(token.FUNC, token.IDENT)
+}
+
+// constStmt : implement stmtDecoder
+type constStmt struct {
+	scanner scanner.Scanner
+	p       interfaces.Parser
+}
+
+func (this *constStmt) decode(endTok token.TokenType) (ast.Statement, error) {
+	stmt := ast.NewConst()
+	if err := this.scanner.ExpectPeek(token.IDENT); nil != err {
+		return nil, function.NewError(err)
+	}
+	stmt.Name = this.scanner.GetIdentifier()
+
+	if builtin.IsBuiltin(stmt.Name.Value) {
+		err := fmt.Errorf("`%v` is built-in function", stmt.Name.Value)
+		return nil, function.NewError(err)
+	}
+
+	if err := this.scanner.ExpectPeek(token.ASSIGN); nil != err {
+		return nil, function.NewError(err)
+	}
+
+	this.scanner.NextToken()
+
+	expr, err := this.p.ParseExpression(scanner.PRECED_LOWEST)
+	if nil != err {
+		return nil, function.NewError(err)
+	}
+	if fn, err := expr.AsFunction(); nil == err {
+		fn.Lambda = stmt.Name.Value
+	}
+	stmt.Value = expr
+
+	for !this.scanner.StmtEnd(endTok) {
+		this.scanner.NextToken()
+	}
+	return stmt, nil
+}
+
+// exprStmt : implement stmtDecoder
+type exprStmt struct {
+	scanner scanner.Scanner
+	p       interfaces.Parser
+}
+
+func (this *exprStmt) decode(endTok token.TokenType) (ast.Statement, error) {
+	stmt := ast.NewExpr()
+	expr, err := this.p.ParseExpression(scanner.PRECED_LOWEST)
+	if nil != err {
+		return nil, function.NewError(err)
+	}
+	stmt.Expr = expr
+
+	if err := this.scanner.PeekIs(token.SEMICOLON); nil == err {
+		this.scanner.NextToken()
+	}
+	return stmt, nil
+}
+
+// functionStmt : implement stmtDecoder
+type functionStmt struct {
+	scanner scanner.Scanner
+	p       interfaces.Parser
+}
+
+func (this *functionStmt) decode(endTok token.TokenType) (ast.Statement, error) {
+	this.scanner.NextToken()
+	stmt := this.scanner.NewFunction()
+	fn, err := this.scanner.ParseFunction(false, this.p)
+	if nil != err {
+		return nil, function.NewError(err)
+	}
+	stmt.Value = fn
+	for !this.scanner.StmtEnd(endTok) {
+		this.scanner.NextToken()
+	}
+	return stmt, nil
 }
