58ebeff3a9a17ba97a81732f6f17c906ff376226
jobs <yao050421103@163.com>
2023-03-23 20:41:03 +0800

    merge builtln

--- builtin/builtin.go
+++ builtin/builtin.go
@@ -1,9 +1,13 @@
 package builtin
 
 import (
+	"encoding/json"
 	"fmt"
 	"strconv"
+	"strings"
 
+	"github.com/jobs-github/escript/function"
+	ejson "github.com/jobs-github/escript/json"
 	"github.com/jobs-github/escript/object"
 )
 
@@ -98,4 +102,96 @@ func newFormatArgs(entry string, args object.Objects) (*formatArgs, error) {
 		return nil, fmt.Errorf("%v the first argument should be string (%v given)", entry, object.Typeof(format))
 	}
 	return &formatArgs{format: unquote(format.String()), args: s}, nil
+}
+
+// implement
+func builtinType(args object.Objects) (object.Object, error) {
+	argc := len(args)
+	if argc != 1 {
+		return object.Nil, fmt.Errorf("type() takes exactly one argument (%v given)", argc)
+	}
+	return object.NewString(object.Typeof(args[0])), nil
+}
+
+func builtinStr(args object.Objects) (object.Object, error) {
+	argc := len(args)
+	if argc != 1 {
+		return object.Nil, fmt.Errorf("str() takes exactly one argument (%v given)", argc)
+	}
+	return object.NewString(args[0].String()), nil
+}
+
+func builtinPrint(args object.Objects) (object.Object, error) {
+	argc := len(args)
+	if argc == 0 {
+		return object.NewString(""), nil
+	}
+	var s strings.Builder
+	for _, arg := range args {
+		s.WriteString(arg.String())
+	}
+	fmt.Print(s.String())
+	return object.NewString(""), nil
+}
+
+func builtinPrintln(args object.Objects) (object.Object, error) {
+	argc := len(args)
+	if argc == 0 {
+		return object.NewString(""), nil
+	}
+	var s strings.Builder
+	for _, arg := range args {
+		s.WriteString(arg.String())
+	}
+	fmt.Println(s.String())
+	return object.NewString(""), nil
+}
+
+func builtinPrintf(args object.Objects) (object.Object, error) {
+	r, err := newFormatArgs("printf()", args)
+	if nil != err {
+		return object.Nil, err
+	}
+	fmt.Printf(r.format, r.args...)
+	return object.NewString(""), nil
+}
+
+func builtinSprintf(args object.Objects) (object.Object, error) {
+	r, err := newFormatArgs("printf()", args)
+	if nil != err {
+		return object.Nil, err
+	}
+	return object.NewString(fmt.Sprintf(r.format, r.args...)), nil
+}
+
+func builtinLoads(args object.Objects) (object.Object, error) {
+	argc := len(args)
+	if argc != 1 {
+		return object.Nil, fmt.Errorf("loads() takes exactly one argument (%v given)", argc)
+	}
+	if !object.IsString(args[0]) {
+		return object.Nil, fmt.Errorf("loads the first argument should be string (%v given)", object.Typeof(args[0]))
+	}
+	s := unquote(args[0].String())
+	v, err := ejson.Decode(s)
+	if nil != err {
+		return object.Nil, err
+	}
+	return v, nil
+}
+
+func builtinDumps(args object.Objects) (object.Object, error) {
+	argc := len(args)
+	if argc != 1 {
+		return object.Nil, fmt.Errorf("dumps() takes exactly one argument (%v given)", argc)
+	}
+	v, err := args[0].Dump()
+	if nil != err {
+		return object.Nil, err
+	}
+	b, err := json.Marshal(v)
+	if nil != err {
+		return object.Nil, err
+	}
+	return object.NewString(function.BytesToString(b)), nil
 }
